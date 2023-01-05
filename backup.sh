95a7e25f5f987a13eb2b8418bb058d6edc58d561
Max Bachmann <kontakt@maxbachmann.de>
2023-01-05 22:28:40 +0100

    better handle errors

--- backup.sh
+++ backup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-set -e
-mount /dev/daily_backup /mnt/daily_backup
-NUMBER=$(snapper -c data create -c number -p -d "Daily Backup")
-rdiff-backup /srv/.snapshots/$NUMBER/snapshot/ /mnt/daily_backup/srv_backup/
-rdiff-backup --remove-older-than 4W --force /mnt/daily_backup/srv_backup/
+
+(
+    set -e
+    mount /dev/daily_backup /mnt/daily_backup
+    NUMBER=$(snapper -c data create -c number -p -d "Daily Backup")
+    rdiff-backup /srv/.snapshots/$NUMBER/snapshot/ /mnt/daily_backup/srv_backup/
+)
+errorCode=$?
+if [ $errorCode -eq 0 ]; then
+    rdiff-backup --remove-older-than 4W --force /mnt/daily_backup/srv_backup/
+fi
 umount /mnt/daily_backup
 hdparm -y /dev/daily_backup
+exit $errorCode
